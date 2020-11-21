2629695fd7f9ce4429c3080d68eecb67096d04ba
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2020-11-21 20:16:42 +0000

    Changed application title and icon

--- src/__test__/Room.test.js
+++ src/__test__/Room.test.js
@@ -1,59 +1,54 @@
 import React from "react";
-import { createMemoryHistory } from "history";
 import { render, screen, fireEvent, getByTestId } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Room from "../components/Room";
 import { BrowserRouter as Router, Route} from 'react-router-dom'
-import useChat from "../ChatHandler";
-import {unmountComponentAtNode} from "react-dom";
 
 
-// let container = null;
-// beforeEach(() => {
-//     let userList = [];
-//     const user1 = {
-//         username: "aName",
-//         senderId: "1DR4nDom",
-//         roomId: "room1"
-//
-//     }
-//     const user2 = {
-//         username: "anotherName",
-//         senderId: "12DR4nDom",
-//         roomId: "room1"
-//
-//     }
-//     userList.push(user1, user2);
-//     console.log(userList)
-// });
-//
-// afterEach(() => {
-// });
-//
-//
-// const renderWithRouter = (ui, { route = '/' } = {}) => {
-//     window.history.pushState({}, 'room1', route)
-//
-//     return render(ui, { wrapper: Router })
-// }
-//
-// test('Testing to see if a user can change their name', () => {
-//     const route = '/room1'
-//     renderWithRouter(<Room match={{params: {roomId: "room1"}, isExact: true, path: "/:roomId", url: "/room1"}}/>, { route })
-//
-//
-//     const leftClick = { button: 0 }
-//     userEvent.type("Please Enter Your Name", "MyName")
-//     userEvent.click(screen.getByText("Set"))
-//
-//     expect(screen.getByText('You\'re in Room: room1')).toBeInTheDocument()
-//
-//     expect(screen.getByText('Active Users: (0)')).toBeInTheDocument()
-//
-//     expect(screen.getByPlaceholderText("Please Enter Your Name")).toBeInTheDocument()
-//
-//     //Test that the input field for typing in a message appears
-//     expect(screen.getByPlaceholderText("Send a message...")).toBeInTheDocument()
-// })
+
+let container = null;
+beforeEach(() => {
+    let userList = [];
+    const user1 = {
+        username: "aName",
+        senderId: "1DR4nDom",
+        roomId: "room1"
+
+    }
+    const user2 = {
+        username: "anotherName",
+        senderId: "12DR4nDom",
+        roomId: "room1"
+
+    }
+    userList.push(user1, user2);
+    console.log(userList)
+});
+
+
+
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+    window.history.pushState({}, 'room1', route)
+
+    return render(ui, { wrapper: Router })
+}
+
+test('Testing to see if a user can change their name', () => {
+    const route = '/room1'
+    renderWithRouter(<Room match={{params: {roomId: "room1"}, isExact: true, path: "/:roomId", url: "/room1"}}/>, { route })
+
+
+    const leftClick = { button: 0 }
+    userEvent.type("Please Enter Your Name", "MyName")
+
+    expect(screen.getByText('You\'re in Room: room1')).toBeInTheDocument()
+
+    expect(screen.getByText('Active Users: (0)')).toBeInTheDocument()
+
+    expect(screen.getByPlaceholderText("Please Enter Your Name")).toBeInTheDocument()
+
+    //Test that the input field for typing in a message appears
+    expect(screen.getByPlaceholderText("Send a message...")).toBeInTheDocument()
+})
 
 
--- src/components/RoomSelection.js
+++ src/components/RoomSelection.js
@@ -10,17 +10,24 @@ const RoomSelection = () => {
     };
 
     return (
-        <div className="roomsel-container">
-            <input
-                type="text"
-                placeholder="Enter the Chat Room You Would Like To Enter"
-                value={roomName}
-                onChange={handleRoomNameChange}
-                className="roomsel-input-field"
-            />
-            <Link to={`/${roomName}`} className="enter-room-button">
-                Join this chat room
-            </Link>
+        <div>
+
+            <div className="home-title">
+                <b>Welcome to <span className="white-text-color">Simple</span> <span className="blue-text-color">Chat     </span></b>
+                <img src="https://icon-library.com/images/chat-app-icon/chat-app-icon-24.jpg"  width="128" height="128"/>
+            </div>
+            <div className="roomsel-container">
+                <input
+                    type="text"
+                    placeholder="Enter the Chat Room You Would Like To Enter"
+                    value={roomName}
+                    onChange={handleRoomNameChange}
+                    className="roomsel-input-field"
+                />
+                <Link to={`/${roomName}`} className="enter-room-button">
+                    Join this chat room
+                </Link>
+            </div>
         </div>
     );
 };
